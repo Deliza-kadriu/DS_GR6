ae176d289a67ff01b835b202aec11dff2bcc1cf7
Deliza-kadriu <57864904+Deliza-kadriu@users.noreply.github.com>
2020-03-25 00:04:57 +0100

    Update BealeCipher.java

--- BealeCipher.java
+++ BealeCipher.java
@@ -1,8 +1,6 @@
 import javax.swing.JOptionPane;
 import java.io.*;
 import java.nio.file.*; 
- 
-
 
 public class BealeCipher {
 	 public static void encrypt(String path,String encryptWord) {
@@ -31,9 +29,7 @@ public class BealeCipher {
 	     	    	    else {
 	     	    	    	t++;
 	     	    	    }
-	     	    	    }
-	     	    	    	    
-	     	    	
+	     	    	    }	     	    	
     	        	if(t < n) {
     		    	System.out.print(count+" ");
     	    	}
@@ -45,15 +41,79 @@ public class BealeCipher {
     	    } 
 
 	 }
+	 public static void decrypt(String path,String decryptWord) {
+			 try {
+	    	      File file = new File(path);
+	    	      if (!file.exists())
+	    	        throw new FileNotFoundException();
+	    	    
+	    	      
+	    	        String s1 = new String(Files.readAllBytes(Paths.get(path)));
+	    	        String s =  s1.toLowerCase();
+	    	    	   int count=0;
+	    	    		for (String st1 :s.split(" ")) {
+	    	    			count++;
+	    	    		}
+	    	    		for(String st : decryptWord.split("\\s+")) {
+	    	    			Boolean bool = isNumeric(st);
+	    	    			int n;
+	    	    			if(bool) {
+	    	    		 n = Integer.parseInt(st);}
+	    	    			else {
+	    	    				n=-1;
+	    	    			}
+	    	    		if(n<count && n>0 && bool) {
+	    	    		char ch = (s.split("\\s+")[n-1]).charAt(0);
+	    	    		System.out.print(ch);
+	    	    		}
+	    	    		else {
+	    	    			continue;
+	    	    		}
+	    	    		}
+	    	
+	    	     
+	    	    } catch (Exception e) {
+	    	      System.err.println(e.getMessage());
+	    	    } 
 
+		 
+	 }
+
+	 //Classen isNumeric e kam marr te gatshme nga faqja : https://www.baeldung.com/java-check-string-number
+		public static boolean isNumeric(String strNum) {
+		    if (strNum == null) {
+		        return false;
+		    }
+		    try {
+		        double d = Double.parseDouble(strNum);
+		    } catch (NumberFormatException nfe) {
+		        return false;
+		    }
+		    return true;
+		}
 	    public static void main(String[] args) 
-	    { 
-	     	String path = JOptionPane.showInputDialog("Provide a path.");
-	     	String encryptWord = JOptionPane.showInputDialog("Provide a word you want to encrypt.");
+	    { 	     	
+	     	    String[] options = new String[] {"1", "2"};
+	     	    int response = JOptionPane.showOptionDialog(null, "Choose 1 for encryption and 2 for decryption","",
+	     	        JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+	     	        null, options, options[0]);
+	     	   String path = JOptionPane.showInputDialog("Provide a path.");
+	     	  	     	
+	     	if(response == 0) {
+	    	String encryptWord = JOptionPane.showInputDialog("Provide a word you want to encrypt.");
+		     	
 	     	System.out.println(path);
 	     	System.out.println(encryptWord);
 	     	encrypt(path, encryptWord);
-	     	 
+	     	} 
+	    	if(response == 1) {
+		    	String decryptWord = JOptionPane.showInputDialog("Provide a word you want to decrypt.");
+			     	
+		     	System.out.println(path);
+		     	System.out.println(decryptWord);
+		     	decrypt(path, decryptWord);
+		     	} 
+	     	
 	     	
 	    }
 }
