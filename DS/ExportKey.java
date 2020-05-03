a578bb6e84f150617dc04fcb3898768ff6dc2269
Deliza Kadriu <57864904+Deliza-kadriu@users.noreply.github.com>
2020-05-04 01:48:29 +0200

    Update ExportKey.java

--- DS/ExportKey.java
+++ DS/ExportKey.java
@@ -1,1 +1,15 @@
 
+import java.io.*;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+
+public class ExportKey {
+
+
+}
+//Metoden printKey dhe convertXMLFileToXMLDocument e kame marre te gatshme nga faqja :
+//https://howtodoinjava.com/xml/xml-to-string-write-xml-file/
