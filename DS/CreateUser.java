b2adb3ccf8a8b2434e1057913905bb6c8e8a3d4d
dorontinaj <58146212+dorontinaj@users.noreply.github.com>
2020-05-04 12:07:53 +0200

    Krijimi i funksionit per celes privat

--- DS/CreateUser.java
+++ DS/CreateUser.java
@@ -33,4 +33,41 @@ public class CreateUser {
             System.out.println("Exception thrown : " + e);
             }
     }
+	public static void PrivateKeyXML(PrivateKey privateKey, String name) throws Exception {
+        try {
+            File file = new File("c://RSAKeys");
+	    file.mkdir();
+	    File filePriv = new File(file.getPath()+"//"+ name +".xml");
+
+	    if (filePriv.exists()){
+		    System.out.print("Gabim: Celesi " + name + " ekziston paraprakisht!");
+	    }		
+	    else if(!filePriv.exists()){
+                KeyFactory keyF = KeyFactory.getInstance("RSA");
+                RSAPrivateCrtKeySpec keyS = keyF.getKeySpec(privateKey, RSAPrivateCrtKeySpec.class);
+
+                //Kthimi nga BigInteger ne Base64
+                BigInteger mod = keyS.getModulus();
+                String modstr = new String(Base64.getEncoder().encodeToString(mod.toByteArray()));
+
+                BigInteger exp = keyS.getPublicExponent();
+                String expstr = new String(Base64.getEncoder().encodeToString(exp.toByteArray()));
+
+                BigInteger prP = keyS.getPrimeP();
+                String prPstr = new String(Base64.getEncoder().encodeToString(prP.toByteArray()));
+
+                BigInteger prQ = keyS.getPrimeQ();
+                String prQstr = new String(Base64.getEncoder().encodeToString(prQ.toByteArray()));
+
+                BigInteger DPp = keyS.getPrimeExponentP();
+                String DPpstr = new String(Base64.getEncoder().encodeToString(DPp.toByteArray()));
+
+                BigInteger DQq = keyS.getPrimeExponentQ();
+                String DQqstr = new String(Base64.getEncoder().encodeToString(DQq.toByteArray()));
+
+                BigInteger inversii = keyS.getCrtCoefficient();
+                String inversiistr = new String(Base64.getEncoder().encodeToString(inversii.toByteArray()));
+                
+                BigInteger de = keyS.getPrivateExponent();
+                String destr = new String(Base64.getEncoder().encodeToString(de.toByteArray()));
 }
