61e8a239a7643ba03e44879de4e9517c1c9cc284
Deliza Kadriu <57864904+Deliza-kadriu@users.noreply.github.com>
2020-05-05 17:18:41 +0200

    Shtimi i metodes export_pub_priv

--- DS/ExportKey.java
+++ DS/ExportKey.java
@@ -70,6 +70,54 @@ public class ExportKey {
 	        }
 	    }
 	 
+	public static void export_pub_priv(String name, String pri_pub,String newFile) {
+		try {
+			File private_key = new File("keys/", name + ".xml");
+			File public_key = new File("keys/", name + ".pub.xml");
+			
+			if (pri_pub.equalsIgnoreCase("public")) {
+				
+				if (public_key.exists()) {
+					Document xmlDoc = convertXMLFileToXMLDocument(String.valueOf(public_key));
+					
+					File pubnew =new File(newFile);
+					pubnew.createNewFile();
+					public_key.delete();
+					printKeyAndWriteKey(xmlDoc,pubnew);
+					System.out.println("Celesi publik u ruajt ne fajllin \"" + newFile + "\"");
+					
+				}
+				else {
+					
+					System.out.println("Gabim : Qelesi publik \"" + name + "\" nuk ekziston");
+				
+				}
+			} 
+			
+			else if (pri_pub.equalsIgnoreCase("private")) {
+				
+				if (private_key.exists()) {
+					Document xmlDoc = convertXMLFileToXMLDocument(String.valueOf(private_key));
+					
+					File pubnew =new File(newFile);
+					pubnew.createNewFile();
+					private_key.delete();
+					printKeyAndWriteKey(xmlDoc,pubnew);
+					System.out.println("Celesi private u ruajt ne fajllin \"" + newFile + "\"");
+				} 
+				else {
+					
+					System.out.println("Gabim : Qelesi privat \"" + name + "\" nuk ekziston");
+
+				}
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+	}
 
 
 }
