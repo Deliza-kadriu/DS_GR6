e8c5591dd351b91132991c21d5aa2979e26cd6fc
Deliza Kadriu <57864904+Deliza-kadriu@users.noreply.github.com>
2020-05-06 23:57:42 +0200

    Update ds.java

--- DS/ds.java
+++ DS/ds.java
@@ -105,14 +105,10 @@ public class ds {
 		
 		else if (args[0].equalsIgnoreCase("read-message")) {
 			try{
-	            if (args.length == 2) {
 	                ReadMessage.read(args[1]);
 	           
-	            } else {
-	                System.out.println("Argumentet nuk jane ne rregull");
-	                System.exit(1);
-	            }
-			}
+	           }
+			
 			catch (Exception e){
 			System.out.println("Nuk keni dhene argumente valide.");
 			}
