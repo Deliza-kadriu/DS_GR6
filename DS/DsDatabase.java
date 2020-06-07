28c3268b496f9d9c81d629855a73938807846568
dorontinaj <58146212+dorontinaj@users.noreply.github.com>
2020-06-07 21:40:34 +0200

    Funksionet per shtim dhe fshirje nga db

--- DS/DsDatabase.java
+++ DS/DsDatabase.java
@@ -3,20 +3,60 @@ import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 import java.security.SecureRandom;
 
-
 public class DsDatabase{
  		
+		public static void shtoNeDB(String name, String pass) throws NoSuchAlgorithmException{
+		      	String url="jdbc:mysql://localhost:3306/databazasd";
+            		String namee="root";
+          		String passwordd="";
+		    try{
+			Connection MyConn= DriverManager.getConnection(url, namee, passwordd);
+			String query="Select * from shfrytesuesit where emri='"+ name +"'";
+             		Statement s = MyConn.createStatement();
+ 			ResultSet rs=s.executeQuery(query);
+               		if (rs.next()==false){
+                 	 byte[] salt = getSalt();
+               		 String passHash = get_SHA_256_SecurePassword(pass, salt);
+               		 String sql="Insert into shfrytesuesit (emri, passwordi, salti)"+ "VALUES ('"+ name +"','"+ passHash +"','"+ salt +"')";
+             	   	 PreparedStatement stmt = MyConn.prepareStatement(sql);
+               		 stmt.executeUpdate(sql);
+               		 System.out.println("Eshte krijuar shfrytezuesi " + name);
+               	        }
+                        else
+                         System.out.print("");
+		     }
+		catch (SQLException err){
+              	    System.out.println(err.getMessage());
+		}
+		}
+	
+		public static void fshijNgaDB(String name){
+           	 String url="jdbc:mysql://localhost:3306/databazasd";
+           	 String namee="root";
+           	 String passwordd="";
+		try {
+                Connection MyConn= DriverManager.getConnection(url, namee, passwordd);
+                String delete = "Delete from shfrytesuesit where emri='" + name + "'";
+                PreparedStatement stmt = MyConn.prepareStatement(delete);
+                stmt.executeUpdate(delete);
+            }
+            catch (SQLException err){
+                System.out.println(err.getMessage());
+            }
+            }
+
 	    public static byte[] getSalt() throws NoSuchAlgorithmException{
-       	 SecureRandom sr = new SecureRandom();
-       	 byte[] salt = new byte[16];
-       	 sr.nextBytes(salt);
-       	 return salt;
+       		 SecureRandom sr = new SecureRandom();
+       		 byte[] salt = new byte[16];
+       		 sr.nextBytes(salt);
+       		 return salt;
    		}
 
-   		public static String get_SHA_256_SecurePassword(String passwordToHash, byte[] salt){
-         String generatedPassword = null;
+   	public static String get_SHA_256_SecurePassword(String passwordToHash, byte[] salt){
+     	String generatedPassword = null;
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
+	    md.reset();
             md.update(salt);
             byte[] bytes = md.digest(passwordToHash.getBytes());
             StringBuilder sb = new StringBuilder();
