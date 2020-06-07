eb587e549fad89112af1b338630d07ff305e4670
Deliza Kadriu <57864904+Deliza-kadriu@users.noreply.github.com>
2020-06-07 23:37:38 +0200

    Gjuajtja e nje exception

--- DS/ds.java
+++ DS/ds.java
@@ -2,7 +2,7 @@ import java.io.Console;
 import java.util.Scanner;
 
 public class ds {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Scanner input=new Scanner(System.in);
 		Console console = System.console();
 		if (args[0].equals("Caesar")) {
