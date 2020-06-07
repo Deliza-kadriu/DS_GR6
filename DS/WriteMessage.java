5200afe65710e4d360af5614a63ab9a0a4b86337
Deliza Kadriu <57864904+Deliza-kadriu@users.noreply.github.com>
2020-06-07 23:12:51 +0200

    Shtimi ne komanden write i sender

--- DS/WriteMessage.java
+++ DS/WriteMessage.java
@@ -2,6 +2,8 @@ import org.w3c.dom.Document;
 import javax.crypto.spec.DESKeySpec;
 import javax.crypto.spec.DESedeKeySpec;
 import java.security.spec.KeySpec;
+import java.security.spec.RSAPrivateKeySpec;
+
 import org.xml.sax.SAXException;
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
@@ -15,6 +17,9 @@ import java.security.*;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.RSAPublicKeySpec;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.Random;
@@ -27,23 +32,24 @@ import java.nio.charset.Charset;
 
 public class WriteMessage {
 
-public static void writemessage(String emri, String mesazhi, String fajlli) throws IOException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, KeyStoreException, InvalidKeySpecException, ParserConfigurationException, SAXException, InvalidAlgorithmParameterException {
-	File filePub = new File("keys/", emri + ".pub.xml");  
+public static void writemessage(String emrii, String mesazhi, String fajlli) throws IOException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, KeyStoreException, InvalidKeySpecException, ParserConfigurationException, SAXException, InvalidAlgorithmParameterException {
+	File filePub = new File("keys/", emrii + ".pub.xml");  
 
     if (!filePub.exists()){
-        System.out.println("Gabim: Celesi publik "+emri+ " nuk ekziston.\n");
+        System.out.println("Gabim: Celesi publik "+emrii+ " nuk ekziston.\n");
     }else {
         
-       String emrii = encode64(emri);
+       String emri = encode64(emrii);
 
 	SecureRandom sr = new SecureRandom();
         byte[] ivD = new byte[8]; 
         sr.nextBytes(ivD);
    
         String iv = encode64(Arrays.toString(ivD));
-        String Dess = encode64(DESenkriptimi(mesazhi));
-        String Rsaa = encode64(RSAenkriptimi(emri));
-        String ciphertext = emrii+"."+iv+"."+Rsaa+"."+Dess;
+        String Dess=DESenkriptimi(mesazhi);
+        String[] parts = Dess.split("-");
+        String Rsaa = RSAenkriptimi(emrii,parts[1].replaceAll("-", "").getBytes());
+        String ciphertext = emri+"."+iv+"."+Rsaa+"."+parts[0].replaceAll("-", "");
         PrintWriter writer = new PrintWriter(fajlli, StandardCharsets.UTF_8);
         writer.println(ciphertext);
         writer.close();
@@ -51,22 +57,139 @@ public static void writemessage(String emri, String mesazhi, String fajlli) thro
 
     }
     }
+public static void write_messageSender(String emrii, String message, String sender) throws Exception {
+    File file = new File("keys/" + emrii + ".pub.xml"); 
+    if (file.exists()) {
+        byte[] bytes = emrii.getBytes("UTF-8");
+        String emri = Base64.getEncoder().encodeToString(bytes);
 
-public static void writemessagee(String emri, String mesazhi) throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException, IOException, ParserConfigurationException, SAXException, InvalidAlgorithmParameterException, UnsupportedEncodingException {
-File filePub = new File("keys/", emri + ".pub.xml");
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = docBuilder.parse(file);
+        doc.getDocumentElement().normalize();
+        SecureRandom sr = new SecureRandom();
+
+        byte[] iv = new byte[8]; 
+        sr.nextBytes(iv);
+
+        String ivi = Base64.getEncoder().encodeToString(iv);
+
+        Random keyi = new Random();
+        byte[] keys = new byte[8];
+        keyi.nextBytes(keys);
+        KeySpec myKeySpec = new DESKeySpec(keys);
+        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DES");
+
+        SecretKey celesi = secretKeyFactory.generateSecret(myKeySpec);
+
+
+        String moduliS =  doc.getElementsByTagName("Modulus").item(0).getTextContent();
+        String exponentAsString =  doc.getElementsByTagName("Exponent").item(0).getTextContent();
+
+        BigInteger modulus = new BigInteger(Base64.getDecoder().decode(moduliS));
+        BigInteger exponent = new BigInteger(Base64.getDecoder().decode(exponentAsString));
+
+        RSAPublicKeySpec keySpec = new RSAPublicKeySpec(modulus, exponent);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey pubKey = keyFactory.generatePublic(keySpec);
+
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+        byte[] encryptedKe = cipher.doFinal(celesi.getEncoded());
+        String encryptedKey = Base64.getEncoder().encodeToString(encryptedKe);
+        Cipher encryptedcipher = Cipher.getInstance("DES");
+        encryptedcipher.init(Cipher.ENCRYPT_MODE, celesi);
+        byte[] utf8 = message.getBytes("UTF8");
+        byte[] enc = encryptedcipher.doFinal(utf8);
+        enc = Base64.getEncoder().encode(enc);
+        String encryptedWord = new String(enc);
+        System.out.println(enc);
+
+        String[] stringss = sender.split("\\.");
+        byte[] tedhenat = Base64.getDecoder().decode(stringss[1]);
+        String data = new String(tedhenat);
+        String nenshkrimi = sender.split("\\.")[2];
+
+        String name = data.split("'")[3];
+
+        String dataSkadimit = data.split("'")[7];
+        long currentTimeMillis = System.currentTimeMillis();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Date resultdate = new Date(currentTimeMillis);
+        String string = sdf.format(resultdate);
+        String name_b64 = Base64.getEncoder().encodeToString(name.getBytes());
+
+        File file1 = new File("keys/" + name + ".xml");
+
+        DocumentBuilderFactory docFactory1 = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder1 = docFactory1.newDocumentBuilder();
+        Document doc1 = docBuilder1.parse(file1);
+        doc1.getDocumentElement().normalize();
+
+        String moduliSp = doc1.getElementsByTagName("Modulus").item(0).getTextContent();
+        String D = doc1.getElementsByTagName("D").item(0).getTextContent();
+
+        BigInteger modulusp = new BigInteger(Base64.getDecoder().decode(moduliSp));
+        BigInteger d = new BigInteger(Base64.getDecoder().decode(D));
+
+
+        RSAPrivateKeySpec keySpec1 = new RSAPrivateKeySpec(modulusp, d);
+        KeyFactory keyFactory1 = KeyFactory.getInstance("RSA");
+        PrivateKey privateKey = keyFactory1.generatePrivate(keySpec1);
+
+        Signature privateSignature = Signature.getInstance("SHA1withRSA");
+        privateSignature.initSign(privateKey);
+        privateSignature.update((encryptedWord).getBytes("UTF_8"));
+
+        byte[] signature = privateSignature.sign();
+
+        String signi = Base64.getEncoder().encodeToString(signature);
+
+
+
+
+        if (Login.verifikosigniture(sender)) {
+
+
+            int compareTO = string.compareTo(dataSkadimit);
+            if (compareTO < 0) {
+                boolean has = Login.gjeje(name);
+                if (has == true) {
+                    System.out.println(emri + "." + ivi + "." + encryptedKey + "." + encryptedWord + "." + name_b64 + "." + signi);
+                } else
+                    System.out.println("Tokeni nuk eshte valid");
+            } else
+                System.out.println("Tokeni nuk eshte valid");
+
+        } else {
+            System.out.println("Tokeni nuk eshte valid");
+        }
+
+
+
+
+    } else {
+        System.out.println("Gabim:Qelesi publik '" + emrii + "' nuk ekziston");
+    }
+}
+
+public static void writemessagee(String emrii, String mesazhi) throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException, IOException, ParserConfigurationException, SAXException, InvalidAlgorithmParameterException, UnsupportedEncodingException {
+File filePub = new File("keys/", emrii + ".pub.xml");
 if (!filePub.exists()){
-        System.out.println("Gabim: Celesi publik "+emri+ " nuk ekziston.\n");
+        System.out.println("Gabim: Celesi publik "+emrii+ " nuk ekziston.\n");
     }else {
-    String emrii= encode64(emri);
-	
+    byte[] bytes = emrii.getBytes("UTF-8");
+    String emri = Base64.getEncoder().encodeToString(bytes);
+
     SecureRandom sr = new SecureRandom();
     byte[] ivD = new byte[8]; 
     sr.nextBytes(ivD);
    
-    String iv = encode64(Arrays.toString(ivD));
-    String Dess=encode64(DESenkriptimi(mesazhi));
-    String Rsaa = encode64(RSAenkriptimi(emri));
-    String ciphertext = emrii+"."+iv+"."+Rsaa+"."+Dess;
+    String ivi = Base64.getEncoder().encodeToString(ivD);
+	String Dess=DESenkriptimi(mesazhi);
+    String[] parts = Dess.split("-");
+    String Rsaa = RSAenkriptimi(emrii,parts[1].replaceAll("-", "").getBytes());
+    String ciphertext = emri+"."+ivi+"."+Rsaa+"."+parts[0].replaceAll("-", "");
     System.out.println(ciphertext);
     }
 }
@@ -82,16 +205,16 @@ public static String DESenkriptimi(String mesazhi) throws InvalidKeySpecExceptio
 
     SecretKey celesi = secretKeyFactory.generateSecret(myKeySpec);
 
-    Cipher cipher = Cipher.getInstance("DES");
-    cipher.init(Cipher.ENCRYPT_MODE, celesi);
-    byte[] utf8 = mesazhi.getBytes("UTF8");
-    byte[] enc = cipher.doFinal(utf8);
-    enc = Base64.getEncoder().encode(enc);
-    String encryptedWord = Base64.getEncoder().encodeToString(enc);
+    Cipher encryptedcipher = Cipher.getInstance("DES");
+            encryptedcipher.init(Cipher.ENCRYPT_MODE, celesi);
+            byte[] utf8 = mesazhi.getBytes("UTF8");
+            byte[] enc = encryptedcipher.doFinal(utf8);
+            enc = Base64.getEncoder().encode(enc);
+            String encryptedWord = new String(enc);
 
-    return encryptedWord;
+    return encryptedWord+"-"+keys;
 }
-public static String RSAenkriptimi(String emri) throws InvalidKeySpecException, UnsupportedEncodingException, InvalidKeyException, IOException, InvalidKeySpecException, BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, SAXException, ParserConfigurationException {
+public static String RSAenkriptimi(String emri,byte[] keys) throws InvalidKeySpecException, UnsupportedEncodingException, InvalidKeyException, IOException, InvalidKeySpecException, BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, SAXException, ParserConfigurationException {
 
 File filePub = new File("keys/", emri + ".pub.xml");
 
@@ -100,29 +223,28 @@ File filePub = new File("keys/", emri + ".pub.xml");
     Document doc = documentBuilder.parse(filePub);
     doc.getDocumentElement().normalize();
 	
-    String moduluss = doc.getElementsByTagName("Modulus").item(0).getTextContent();
-    String exponentt = doc.getElementsByTagName("Exponent").item(0).getTextContent();
+    String moduluss = ((org.w3c.dom.Document) doc).getElementsByTagName("Modulus").item(0).getTextContent();
+    String exponentt = ((org.w3c.dom.Document) doc).getElementsByTagName("Exponent").item(0).getTextContent();
 	
     BigInteger modulus = new BigInteger(Base64.getDecoder().decode(moduluss));
     BigInteger exponent = new BigInteger(Base64.getDecoder().decode(exponentt));
 
-    Random keyy = new Random();
-    byte[] keys = new byte[8];
-    keyy.nextBytes(keys);
-    KeySpec myKeySpec = new DESKeySpec(keys);
-    SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DES");
-    SecretKey celesi = secretKeyFactory.generateSecret(myKeySpec);
-	
     
     RSAPublicKeySpec keySpec = new RSAPublicKeySpec(modulus, exponent);
     KeyFactory keyFactory = KeyFactory.getInstance("RSA");
     PublicKey pubKey = keyFactory.generatePublic(keySpec);
+    
+	KeySpec myKeySpec = new DESKeySpec(keys);
+    SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DES");
+
+    SecretKey celesi = secretKeyFactory.generateSecret(myKeySpec);
 
 
  	Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, pubKey);
-        byte[] encryptedKEY = cipher.doFinal(celesi.getEncoded());
-	String encryptedKEYY= Base64.getEncoder().encodeToString(encryptedKEY);
-	return encryptedKEYY;
+            cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+            byte[] encryptedKe = cipher.doFinal(celesi.getEncoded());
+            String encryptedKey = Base64.getEncoder().encodeToString(encryptedKe);
+
+			return encryptedKey;
 }
 }
